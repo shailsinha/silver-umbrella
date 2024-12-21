136235752816ea67e2221ded0290b70c9fbf64f7
shailsinha <shailendra.sinha@mphasis.com>
2024-12-21 06:44:58 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,3 +1,12 @@
-function skillMembers() {
-    console.log("Skill members");
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 30,
+        skills: ["HTML", "CSS", "JS"],
+        greet: function() {
+            console.log("Hello, I'm " + this.name);
+        }
+    }
+    return member;
+        
 }
